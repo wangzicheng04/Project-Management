8894f08850f2cadc3ef60a2aac81b630ca3d4562
HiddenL0ve <1161527727@qq.com>
2025-06-19 17:26:52 +0800

    add water_quality display

--- app/__init__.py
+++ app/__init__.py
@@ -21,9 +21,11 @@ def create_app():
     from .routes import auth_bp
     from .routes import main_bp
     from .routes import weather_bp  # 添加天气蓝图导入
+    from .routes.water_data import data_bp
     
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(weather_bp)  # 注册天气蓝图
+    app.register_blueprint(data_bp)
 
     return app
